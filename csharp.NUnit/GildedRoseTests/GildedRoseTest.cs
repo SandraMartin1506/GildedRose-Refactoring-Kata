dbc926397bca21f3a9a1f7e5dce15e408a901fa0
SandraMartin1506 <sandramcmp@gmail.com>
2026-06-01 00:36:06 +0200

    Update GildedRoseTest.cs

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -9,10 +9,11 @@ public class GildedRoseTest
     [Test]
     public void Foo()
     {
-        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 49 } };
+        var items = new List<Item> { new Item { Name = "foo", SellIn = -1, Quality = -1 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Quality, Is.EqualTo(50));
-        Assert.That(items[0].SellIn, Is.EqualTo(-1));
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+        Assert.That(items[0].SellIn, Is.EqualTo(-2));
+
     }
 }
